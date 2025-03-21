e345393acc863812d60672c1ab680d904a6c8260
svet <kiseleva03@inbox.ru>
2025-03-22 00:21:08 +0300

    rewrite MySQL

---
+++ get_data.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+def weather_data_get(name_city):
+    '''
+    Функция получает название города и возвращает по городу данные из таблицы WeatherData
+    '''
+
+    conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="your password",
+            database= 'Weather'
+        )
+    cursor = conn.cursor()
+    cursor.execute("SELECT city_id FROM City WHERE name_city = %s", (name_city,))
+    city_id=cursor.fetchone()
+    if city_id:
+        cursor.execute("""SELECT * FROM WeatherData WHERE city_id = %s""", (city_id[0],))
+        weather_data = cursor.fetchall()
+        print(weather_data)
+        return weather_data
+    else:
+        print(f'Город {city_name} не найден')
+    cursor.close()
+    conn.close()
+
+def users_requests_get():
+    '''
+    Функция получает ... и возвращает  данные из таблицы UsersRequests
+    '''
+    pass
+
+def users_recommendation_get():
+    '''
+    Функция получает ... и возвращает  данные из таблицы UsersRecommendation
+    '''
+    pass
+
---
+++ write_data.py
@@ -0,0 +1,67 @@
+import mysql.connector
+
+def weather_data_write():
+    '''
+    Функция получает ... и записывает данные по городу в таблицу WeatherData
+    '''
+    pass
+
+def users_profilies_write(username,  password):
+    conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="your password",
+            database= 'Weather'
+        )
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""INSERT INTO UsersProfiles (username, password) VALUES (%s, %s)""", (username,  password))
+        conn.commit()
+    except mysql.connector.IntegrityError:
+        print(f"Пользователь с ником {username}  уже существует")
+    cursor.close()
+    conn.close()
+
+def users_requests_write(request_id, user_id, request_city_id, recommendation_id):
+    '''
+    Функция получает ... и записывает данные в таблицу UsersRequests
+    '''
+    pass
+
+def users_recommendation_write(recommendation):
+    '''
+    Функция записывает recommendation в таблицу UsersRecommendation
+    '''
+    conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="your password",
+            database= 'Weather'
+        )
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""INSERT INTO UsersRecommendation (recommendation) VALUES (%s)""", (recommendation))
+        conn.commit()
+    except mysql.connector.IntegrityError:
+        print(f"Рекомендация  {recommendation} уже существует в таблице UsersRecommendation")
+
+users_profilies_write("test_user", "test_password")
+'''
+conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="your password",
+            database= 'Weather'
+        )
+cursor = conn.cursor()
+
+query = """SELECT * FROM UsersProfiles"""
+cursor.execute(query)
+rows = cursor.fetchall()
+column_names = [description[0] for description in cursor.description]
+print("\t".join(column_names))
+for row in rows:
+    print("\t".join(map(str, row)))
+cursor.close()
+conn.close()
+'''
