0a7ea85b00a125c88ac4ffd92f1de665a1be6655
svet <101479885+ssssveta@users.noreply.github.com>
2025-04-23 17:55:13 +0300

    add get_city_coordinates

--- get_data.py
+++ get_data.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import pandas as pd
 
 def weather_data_get(name_city):
     '''
@@ -9,8 +8,8 @@ def weather_data_get(name_city):
     conn = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="svetlana2003!",
-            database= 'WeatherDB'
+            password="your password",
+            database= 'Weather'
         )
     cursor = conn.cursor()
     cursor.execute("SELECT city_id FROM City WHERE name_city = %s", (name_city,))
@@ -18,46 +17,16 @@ def weather_data_get(name_city):
     if city_id:
         cursor.execute("""SELECT * FROM WeatherData WHERE city_id = %s""", (city_id[0],))
         weather_data = cursor.fetchall()
-        columns = [desc[0] for desc in cursor.description]
         print(weather_data)
-        return pd.DataFrame(weather_data, columns=columns)
+        return weather_data
     else:
         print(f'Город {city_name} не найден')
     cursor.close()
     conn.close()
 
-def weather_data_get():
-    '''
-    Функция возвращает  данные из таблицы WeatherData (return pandas table):
-     city_id INT, time DATETIME, tavg DOUBLE, tmin DOUBLE, tmax DOUBLE, prcp DOUBLE, snow DOUBLE, wdir DOUBLE,
-     wspd DOUBLE, wpgt DOUBLE, pres DOUBLE, tsun DOUBLE,
-    '''
-
-    conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="svetlana2003!",
-            database= 'WeatherDB'
-        )
-
-    try:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM WeatherData ")
-        weather_data=cursor.fetchall()
-        columns = [desc[0] for desc in cursor.description]
-        print(pd.DataFrame(weather_data, columns=columns))
-        print(pd.DataFrame(weather_data, columns=columns))
-        return pd.DataFrame(weather_data, columns=columns)
-
-    except mysql.connector.Error as e:
-        print(f"Ошибка БД: {e}")
-        return pd.DataFrame()
-    cursor.close()
-    conn.close()
-
 def users_requests_get():
     '''
-    Функция получает ... и возвращает  данные из таблицы UsersRequests (предпочтения пользователя)
+    Функция получает ... и возвращает  данные из таблицы UsersRequests
     '''
     pass
 
@@ -67,4 +36,3 @@ def users_recommendation_get():
     '''
     pass
 
-weather_data_get()
